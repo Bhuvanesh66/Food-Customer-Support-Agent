fc7337cd59c2eb16b5747ab792834cb342db7c37
Joe-Daniel29 <very.joe.daniel@gmail.com>
2026-06-07 12:17:00 +0530

    feat(server): id and time helpers

---
+++ server/src/util/id.ts
@@ -0,0 +1,10 @@
+import { nanoid } from 'nanoid';
+
+export function newId(prefix?: string): string {
+  const id = nanoid(16);
+  return prefix ? `${prefix}_${id}` : id;
+}
+
+export function now(): number {
+  return Date.now();
+}
